7a0393c7a29e4fc107485f4dc827be031b56d141
theblindprophet <theblndprophet@gmail.com>
2018-08-02 06:24:17 -0400

    placeholder contributions

--- src/components/Home-Contributions.js
+++ src/components/Home-Contributions.js
@@ -1,11 +1,67 @@
 import React, { Component } from 'react';
 import './Home-Contributions.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class HomeContributions extends Component {
   render() {
     return (
       <div className="Contributions col">
-        Contributions
+        <div className="Contributions-contr">
+          <div className="Contributions-contr-icon">
+            <FontAwesomeIcon icon="code" />
+          </div>
+          <div className="Contributions-contr-summary">
+            Create commit in repo, <b>Apples</b>
+          </div>
+          <div className="Contributions-contr-date">
+            July 30, 2018
+          </div>
+        </div>
+
+        <div className="Contributions-contr">
+          <div className="Contributions-contr-icon">
+            <FontAwesomeIcon icon="code" />
+          </div>
+          <div className="Contributions-contr-summary">
+            Create commit in repo, <b>Apples</b>
+          </div>
+          <div className="Contributions-contr-date">
+            July 30, 2018
+          </div>
+        </div>
+        <div className="Contributions-contr">
+          <div className="Contributions-contr-icon">
+            <FontAwesomeIcon icon="code" />
+          </div>
+          <div className="Contributions-contr-summary">
+            Create commit in repo, <b>Apples</b>
+          </div>
+          <div className="Contributions-contr-date">
+            July 30, 2018
+          </div>
+        </div>
+        <div className="Contributions-contr">
+          <div className="Contributions-contr-icon">
+            <FontAwesomeIcon icon="code" />
+          </div>
+          <div className="Contributions-contr-summary">
+            Create commit in repo, <b>Apples</b>
+          </div>
+          <div className="Contributions-contr-date">
+            July 30, 2018
+          </div>
+        </div>
+        <div className="Contributions-contr">
+          <div className="Contributions-contr-icon">
+            <FontAwesomeIcon icon="code" />
+          </div>
+          <div className="Contributions-contr-summary">
+            Create commit in repo, <b>Apples</b>
+          </div>
+          <div className="Contributions-contr-date">
+            July 30, 2018
+          </div>
+        </div>
       </div>
     );
   }
