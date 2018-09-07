afedfc9bfd112c36993255447730c2d6990b513a
Gross Jamie <jamie.gross@partner.bmwgroup.com>
2018-09-07 12:10:45 -0400

    - Populate user information into components - Provider eror and load feedback

--- src/components/Home-Search-Bar.js
+++ src/components/Home-Search-Bar.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import { IconContext } from "react-icons";
+import { FaSpinner  } from 'react-icons/fa';
 import './Home-Search-Bar.css';
 
 class HomeSearchBar extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { username: '' };
+    this.state = {
+      username: ''
+    };
 
-    this.getUser = this.getUser.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.getUser = this.getUser.bind(this);
+    this.getSubmitButton = this.getSubmitButton.bind(this);
   }
 
   handleChange = (event) => {
@@ -22,14 +27,32 @@ class HomeSearchBar extends Component {
     }
   }
 
+  getSubmitButton = () => {
+    if (this.props.loadingUser) {
+      return (
+        <button className="Search-Bar-form-submit" type="submit">
+          Search
+          <IconContext.Provider value={{ color: "#fff" }}>
+            <div>
+              <FaSpinner />
+            </div>
+          </IconContext.Provider>
+        </button>
+      );
+    }
+    return (
+      <button className="Search-Bar-form-submit" type="submit">
+        Search
+      </button>
+    );
+  }
+
   render() {
     return (
       <div className="Search-Bar row">
         <form className="Search-Bar-form col" onSubmit={this.getUser}>
           <input className="Search-Bar-form-search" type="text" aria-label="search user" placeholder="Search user" value={this.state.username} onChange={this.handleChange}/>
-          <button className="Search-Bar-form-submit" type="submit">
-            Search
-          </button>
+          {this.getSubmitButton()}
         </form>
       </div>
     );
