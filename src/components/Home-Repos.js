714a18598c3ae7b2b8d2f2ee843162029c46f728
theblindprophet <theblndprophet@gmail.com>
2018-09-09 12:02:35 -0400

    Repo list, labels

--- src/components/Home-Repos.js
+++ src/components/Home-Repos.js
@@ -3,48 +3,109 @@ import './Home-Repos.css';
 import { IconContext } from "react-icons";
 import { GoRepo } from 'react-icons/go';
 
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+
 class HomeRepos extends Component {
-  render() {
+
+  constructor(props) {
+    super(props);
+
+    this.createRepos = this.createRepos.bind(this);
+    this.createRepo = this.createRepo.bind(this);
+    this.moreRepos = this.moreRepos.bind(this);
+  }
+
+  createRepos = () => {
+    if (!this.props.userData || !this.props.userData.repositories) {
+      return (
+        <div className="Repos col">
+          <div className="Repos-repo">
+            <IconContext.Provider value={{}}>
+              <div className="Repos-repo-icon">
+                <GoRepo />
+              </div>
+            </IconContext.Provider>
+            <p className="Repos-repo-name">A list of repositories...</p>
+          </div>
+        </div>
+      );
+    }
+    const repos = this.props.userData.repositories.edges;
     return (
       <div className="Repos col">
-        <div className="Repos-repo">
-          <IconContext.Provider value={{}}>
-            <div className="Repos-repo-icon">
-              <GoRepo />
-            </div>
-          </IconContext.Provider>
-          <a className="Repos-repo-name" href="https://www.github.com" target="_blank" rel="noopener noreferrer">Repo name</a>
-          <div className="Repos-repo-updated">Last Updated: <b>July 31, 2018</b></div>
-        </div>
-        <div className="Repos-repo">
-          <IconContext.Provider value={{}}>
-            <div className="Repos-repo-icon">
-              <GoRepo />
-            </div>
-          </IconContext.Provider>
-          <a className="Repos-repo-name" href="https://www.github.com" target="_blank" rel="noopener noreferrer">Repo name</a>
-          <div className="Repos-repo-updated">Last Updated: <b>July 31, 2018</b></div>
-        </div>
-        <div className="Repos-repo">
-          <IconContext.Provider value={{}}>
-            <div className="Repos-repo-icon">
-              <GoRepo />
-            </div>
-          </IconContext.Provider>
-          <a className="Repos-repo-name" href="https://www.github.com" target="_blank" rel="noopener noreferrer">Repo name</a>
-          <div className="Repos-repo-updated">Last Updated: <b>July 31, 2018</b></div>
-        </div>
-        <div className="Repos-repo">
-          <IconContext.Provider value={{}}>
-            <div className="Repos-repo-icon">
-              <GoRepo />
-            </div>
-          </IconContext.Provider>
-          <a className="Repos-repo-name" href="https://www.github.com" target="_blank" rel="noopener noreferrer">Repo name</a>
-          <div className="Repos-repo-updated">Last Updated: <b>July 31, 2018</b></div>
-        </div>
+        {
+          repos.map(({ node }, index) => {
+            return this.createRepo(node, index);
+          })
+        }
+        {this.moreRepos()}
       </div>
     );
+  }
+
+  createRepo = (node, index) => {
+    let dateFormatted;
+    if (node.updatedAt) {
+      const date = new Date(node.updatedAt);
+      dateFormatted = `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+    } else {
+      dateFormatted = 'Unknown';
+    }
+    return (
+      <div className="Repos-repo" key={index}>
+        <IconContext.Provider value={{}}>
+          <div className="Repos-repo-icon">
+            <GoRepo />
+          </div>
+        </IconContext.Provider>
+        <a className="Repos-repo-name" href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
+        <div className="Repos-repo-updated">Last Updated: <b>{dateFormatted}</b></div>
+        {this.createLanguages(node.languages.edges)}
+      </div>
+    );
+  }
+
+  moreRepos = () => {
+    if (!this.props.userData || !this.props.userData.repositories) {
+      return '';
+    } else if (this.props.userData.repositories.totalCount <= 30) {
+      return '';
+    }
+    const url = `${this.props.userData.url}?tab=repositories`;
+    return (
+      <div className="Repos-repo" key="30">
+        <IconContext.Provider value={{}}>
+          <div className="Repos-repo-icon">
+            <GoRepo />
+          </div>
+        </IconContext.Provider>
+        <a className="Repos-repo-name" href={url} target="_blank" rel="noopener noreferrer">View all repositiories...</a>
+      </div>
+    );
+  }
+
+  createLanguages = (languages) => {
+    if (!languages || languages.length === 0) {
+      return (
+        ''
+      )
+    }
+    return (
+      <div className="Repos-repo-languages">
+        {
+          languages.map(({ node }, index) => (
+            <div className="Repos-repo-languages-lang" style={{borderColor: node.color}} key={index}>
+              {node.name}
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
+
+  render() {
+    return this.createRepos();
   }
 }
 
