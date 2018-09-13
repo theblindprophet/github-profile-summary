fe0e8bda6f8229a9051651f07f44bc0700e4c49f
Gross Jamie <jamie.gross@partner.bmwgroup.com>
2018-09-13 08:08:22 -0400

    Add footer, stub for links

--- src/App.js
+++ src/App.js
@@ -26,6 +26,27 @@ class App extends Component {
             <Route exact path="/" component={Home} />
           </Router>
         </div>
+        <footer className="App-footer">
+          <ul className="App-footer-list">
+            <li>
+              <p>
+                Contact Us<br />
+                <a href="mailto:jamie.gross@graspmobiledevelopment.com">jamie.gross@graspmobiledevelopment.com</a><br />
+                <a href="mailto:contact@thewillg.com">contact@thewillg.com</a>
+              </p>
+            </li>
+          </ul>
+          <ul className="App-footer-list">
+            <li>
+              <p>
+                &#169; 2018 githubprofilesummary.com
+                <br />
+                <a href="google.com" target="_blank" rel="noopener noreferrer">Privacy Policy</a><br />
+                <a href="google.com" target="_blank" rel="noopener noreferrer">Terms of Service</a>
+              </p>
+            </li>
+          </ul>
+        </footer>
       </div>
     );
   }
