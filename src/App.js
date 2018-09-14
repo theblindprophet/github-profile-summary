9454ac7be01fcf9ad1ccc0003b29f954b2eb6f73
Jamie Gross <theblindprophet@users.noreply.github.com>
2018-09-14 15:25:44 -0400

    Feature/footer (#24)
    * Add footer, stub for links
    
    * footer page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Home from './Home.js';
+import TermsAndService from './TermsAndPrivacy.js';
 import logo from './assets/github-logo.png';
 import ReactGA from 'react-ga';
 
@@ -19,13 +20,37 @@ class App extends Component {
           <div>
             <span>Integrated with </span><img src={logo} className="App-header-logo" alt="logo" />
           </div>
-          <h1 className="App-header-title">Github Profile Summary</h1>
+          <a href="/"><h1 className="App-header-title">Github Profile Summary</h1></a>
         </header>
         <div className="App-container">
           <Router>
-            <Route exact path="/" component={Home} />
+            <div>
+              <Route exact path="/" component={Home} />
+              <Route path="/terms-and-privacy" component={TermsAndService} />
+            </div>
           </Router>
         </div>
+        <footer className="App-footer">
+          <ul className="App-footer-list">
+            <li>
+              <p>
+                Contact Us<br />
+                <a href="mailto:jamie.gross@graspmobiledevelopment.com">jamie.gross@graspmobiledevelopment.com</a><br />
+                <a href="mailto:contact@thewillg.com">contact@thewillg.com</a>
+              </p>
+            </li>
+          </ul>
+          <ul className="App-footer-list">
+            <li>
+              <p>
+                &#169; 2018 githubprofilesummary.com
+                <br />
+                <a href="/terms-and-privacy#terms">Terms of Service</a><br />
+                <a href="/terms-and-privacy#privacy">Privacy Policy</a>
+              </p>
+            </li>
+          </ul>
+        </footer>
       </div>
     );
   }
